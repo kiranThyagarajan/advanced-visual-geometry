c7cf6f55c99c737f54d17eb395283631b19cdac5
KiranThyagarajan <af2kiran2002@gmail.com>
2025-12-19 11:53:45 +0100

    finished part 2

--- lab2/lab2_stereo.py
+++ lab2/lab2_stereo.py
@@ -23,26 +23,35 @@ disparity = stereo.compute(left, right)  # an image of the same size with "left"
 # TODO: compute depth of every pixel whose disparity is positive
 # hint: assume d is the disparity of pixel (u, v)
 # hint: the depth Z of this pixel is Z = Bf / d
-valid_pixels = disparity > 0 
-Z = np.zeros_like(disparity, dtype=np.float32)
-np.divide(Bf, disparity, out=Z, where=valid_pixels)
-print("Depth map computed.", Z)
+pixels_with_disparity = np.argwhere(disparity > 0)
+logging.info('pixels_with_disparity.shape=%s',pixels_with_disparity.shape)
+
+pixels_disparities = disparity[pixels_with_disparity[:, 0], pixels_with_disparity[:, 1]]
+pixels_with_disparity = np.hstack((pixels_with_disparity, pixels_disparities[:, np.newaxis]))
+logging.info('pixels_with_disparity.shape=%s',pixels_with_disparity.shape)
+Z = Bf / pixels_with_disparity[:, 2] 
+
 # TODO: compute normalized coordinate of every pixel whose disparity is positive
 # hint: the normalized coordinate of pixel [u, v, 1] is K^(-1) @ [u, v, 1]
+normalized_coordinates = np.linalg.inv(K) @ np.hstack((pixels_with_disparity[:, :2], np.ones((pixels_with_disparity.shape[0], 1)))).T
+logging.info('normalized_coordinates.shape=%s',normalized_coordinates.shape)
 
 # TODO: compute 3D coordinate of every pixel whose disparity is positive
 # hint: 3D coordinate of pixel (u, v) is the product of Z and its normalized cooridnate
 
-all_3d = np.array([])  # this is matrix storing 3D coordinate of every pixel whose disparity is positive
+all_3d = Z[: , np.newaxis] * normalized_coordinates.T # this is matrix storing 3D coordinate of every pixel whose disparity is positive
 # the shape of all_3d is <num_pixels_positive_disparity, 3>
 # each row of all_3d is a 3D coordinate [X, Y, Z]
 # you need to change the value of all_3d with your computation of 3D coordinate of every pixel whose disparity is positive
+logging.info('all_3d.shape=%s',all_3d.shape)
+
 
 # TODO: get color for 3D points
-all_color = np.array([])  # this is matrix storing color of every pixel whose disparity is positive
+all_color = left_color[pixels_with_disparity[:, 0], pixels_with_disparity[:, 1]]  # this is matrix storing color of every pixel whose disparity is positive
 # TODO: THE ORDER OF all_color IS THE SAME WITH all_3d
 # the shape of all_color is <num_pixels_positive_disparity, 3>
 # each row of all_color is [R, G, B] value
+logging.info('all_color.shape=%s',all_color.shape)
 
 # normalize all_color
 all_color = all_color.astype(np.float) / 255.0
